d0ee8e475f74f4a15f898a196d705ea3be9f187d
dnyanesh12345678 <90817474+dnyanesh12345678@users.noreply.github.com>
2022-03-05 12:35:11 +0530

    create 4 instance

--- vars.tf
+++ vars.tf
@@ -3,9 +3,9 @@ default = "ap-south-1"
 }
 variable instance_data {
 default = {
-count = "2"
+count = "4"
 ami = "ami-0851b76e8b1bce90b"
 type = "t2.micro"
-tag = ["node1", "node2" ]
+tag = ["control-node", "node1", "node2", "node4" ]
 }
 }
