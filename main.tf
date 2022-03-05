b43c35036f4a9c06a1b0c3db1b365fc92ed2b89e
dnyanesh12345678 <90817474+dnyanesh12345678@users.noreply.github.com>
2022-03-05 12:37:36 +0530

    ansible key added

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 region = var.AWS_REGION
 }
 resource "aws_instance" "my_instance" {
+key_name = "ansible"
 ami = var.instance_data.ami
 instance_type= var.instance_data.type
 count = var.instance_data.count
